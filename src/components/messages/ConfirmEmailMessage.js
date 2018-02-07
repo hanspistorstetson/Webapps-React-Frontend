a7b9420bbbfef8845518f16b63b8f34ed99533a4
Hans Pistor <hpistor@stetson.edu>
2018-02-07 13:34:07 -0500

    Route authorization

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Message } from "semantic-ui-react";
+
+const ConfirmEmailMessage = () => (
+	
+	<div>
+		<Message info>
+			<Message.Header>Please, verify your email to unlock awesomeness</Message.Header>
+		</Message>
+	</div>
+
+
+);
+
+export default ConfirmEmailMessage
